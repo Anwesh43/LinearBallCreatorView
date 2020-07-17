7d8db9384c9b770d1abdb678df15a49b65a88e60
Anwesh43 <anweshthecool0@gmail.com>
2020-07-17 19:23:14 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linearballcreatorview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linearballcreatorview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linearballcreatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.ballcreatorview.BallCreatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallCreatorView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
